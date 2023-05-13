1aac4e8b42d8934d5258d023896bd57b2abb59a6
dutysniper <natarov.constantin@yandex.ru>
2023-05-13 22:39:10 +0300

    create searchBar+
    + create keyboard methods
    + some fixes

--- FakeAPI/ViewControllers/ContactsViewController.swift
+++ FakeAPI/ViewControllers/ContactsViewController.swift
@@ -8,52 +8,88 @@
 import UIKit
 
 final class ContactsViewController: UITableViewController {
-
+    
     //MARK: - Private properties
     private let networkManager = NetworkManager.shared
     private var contacts: [Person] = []
     private var contactsDictionary: [String: [Person]] = [:]
     private var sectionTitles: [String] = []
-
+    private let searchController = UISearchController(searchResultsController: nil)
+    
+    
     //MARK: - life-cycle vc
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchContacts()
+        groupContactsByLastName()
+        setupToolbar()
+        setupSearchController()
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tapGesture.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapGesture)
+        
     }
-
+    
     //MARK: - TableView DataSource methods
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return sectionTitles.count
+        if searchController.isActive {
+            return sectionTitles.count
+        } else {
+            return contactsDictionary.keys.count
+        }
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let sectionTitle = sectionTitles[section]
-        return contactsDictionary[sectionTitle]?.count ?? 0
+        if let sectionContacts = contactsDictionary[sectionTitle], searchController.isActive {
+            return sectionContacts.count
+        } else {
+            return contactsDictionary[sectionTitle]?.count ?? 0
+        }
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell") as? ContactCell else {
             return UITableViewCell()
         }
         let sectionTitle = sectionTitles[indexPath.section]
         let sectionContacts = contactsDictionary[sectionTitle] ?? []
-        cell.configure(with: sectionContacts[indexPath.row])
+        let contact = sectionContacts[indexPath.row]
+        cell.configure(with: contact)
         return cell
     }
-
+    
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sectionTitles[section]
+        if searchController.isActive {
+            return sectionTitles[section]
+        } else {
+            let sortedKeys = contactsDictionary.keys.sorted()
+            return sortedKeys[section]
+        }
     }
-
+    
     override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
-        return sectionTitles
+        if searchController.isActive {
+            return sectionTitles
+        } else {
+            let sortedKeys = contactsDictionary.keys.sorted()
+            return sortedKeys.map { String($0.prefix(1)) }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       
+        performSegue(withIdentifier: "showContactDetails", sender: nil)
     }
 
     //MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let detailsVC = segue.destination as? DetailsViewController else { return }
-        let selectedPerson = sender as? Person
-        detailsVC.contact = selectedPerson
+        guard let indexPath = tableView.indexPathForSelectedRow else { return }
+            let sectionTitle = sectionTitles[indexPath.section]
+            let sectionContacts = contactsDictionary[sectionTitle] ?? []
+            let selectedPerson = sectionContacts[indexPath.row]
+            detailsVC.contact = selectedPerson
     }
 }
 
@@ -71,7 +107,10 @@ extension ContactsViewController {
             }
         }
     }
+}
 
+//MARK: - GroupingContacts method
+extension ContactsViewController {
     private func groupContactsByLastName() {
         contactsDictionary = Dictionary(grouping: contacts) { contact in
             guard let firstLetter = contact.lastname.first else {
@@ -80,5 +119,55 @@ extension ContactsViewController {
             return String(firstLetter)
         }
         sectionTitles = contactsDictionary.keys.sorted()
+    }
+}
+
+//MARK: - UISearchResultsUpdating
+extension ContactsViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        if let searchText = searchController.searchBar.text, !searchText.isEmpty {
+            let filteredContacts = contacts.filter { contact in
+                return contact.fullname.lowercased().contains(searchText.lowercased())
+            }
+            contactsDictionary = Dictionary(grouping: filteredContacts) { contact in
+                guard let firstLetter = contact.lastname.first else {
+                    return " "
+                }
+                return String(firstLetter)
+            }
+        } else {
+            groupContactsByLastName()
+        }
+        sectionTitles = contactsDictionary.keys.sorted()
+        tableView.reloadData()
+    }
+}
+
+extension ContactsViewController {
+    private func setupToolbar() {
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 44))
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(dismissKeyboard))
+        toolbar.setItems([flexibleSpace, doneButton], animated: true)
+        searchController.searchBar.inputAccessoryView = toolbar
+    }
+}
+
+
+//MARK: - Keyboard methods
+extension ContactsViewController {
+    @objc private func dismissKeyboard() {
+        searchController.searchBar.resignFirstResponder()
+    }
+}
+
+extension ContactsViewController {
+    private func setupSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        definesPresentationContext = true
     }
 }
