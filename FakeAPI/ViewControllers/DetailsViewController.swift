5415463bc5b8a7931525483a1b9608e15593a606
dutysniper <natarov.constantin@yandex.ru>
2023-05-13 20:28:21 +0300

    create activityIndicator
    //

--- FakeAPI/ViewControllers/DetailsViewController.swift
+++ FakeAPI/ViewControllers/DetailsViewController.swift
@@ -25,19 +25,35 @@ final class DetailsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureUI()
+       let spinner = showSpinner(in: view)
+        configureUI() { [weak spinner] in
+            spinner?.stopAnimating()
+        }
     }
     
     //MARK: - Setup UI
-    private func configureUI() {
+    private func configureUI(completion: @escaping() -> Void) {
         contactInfo.text = contact?.description
         networkManager.fetchData(from: URL(string: contact.image)!) { [weak self] result in
             switch result {
             case .success(let imageData):
                 self?.contactImage.image = UIImage(data: imageData)
+                completion()
             case .failure(let error):
                 print(error)
+                completion()
             }
         }
+    }
+    private func showSpinner(in view: UIView) -> UIActivityIndicatorView {
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        activityIndicator.color = .gray
+        activityIndicator.startAnimating()
+        activityIndicator.center = view.center
+        activityIndicator.hidesWhenStopped = true
+        
+        view.addSubview(activityIndicator)
+        
+        return activityIndicator
     }
 }
